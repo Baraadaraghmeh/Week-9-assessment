1b50ac03b2b97492423bc1e6386af81719d5d64a
Baraadaraghmeh <51743582+Baraadaraghmeh@users.noreply.github.com>
2019-08-08 18:11:36 +0300

    Add files via upload

---
+++ src/UserInput.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+class UserInput extends Component {
+    state = { 
+
+
+     }
+      
+
+         
+         
+     
+    render() { 
+        return (  
+<div>
+    <input type={Text}  > </input>
+</div>
+
+
+        );
+    }
+}
+ 
+export default UserInput;
---
+++ src/Useroutpot.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import UserInput from ''
+class UserOutput extends Component {
+    state = { 
+
+
+     }
+    render() { 
+return (  
+<div>
+<p>
+
+    
+</p>
+<p>
+
+
+</p>
+
+</div>
+
+
+        );
+    }
+}
+ 
+export default UserOutput;
